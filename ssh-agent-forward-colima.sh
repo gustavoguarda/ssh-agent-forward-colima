18e170310a64f4eb2b0f420fced706c32124756d
Gustavo Guarda <gustavoguarda@gmail.com>
2024-05-27 18:07:29 -0300

    feat: add SSH keys to all files in the folder

--- ssh-agent-forward-colima.sh
+++ ssh-agent-forward-colima.sh
@@ -10,14 +10,25 @@ colima stop
 # Start the SSH agent
 eval "$(ssh-agent -s)"
 
-# Add SSH key
-ssh-add ~/.ssh/id_rsa
+DIR="$HOME/.ssh"
+if [ -d "$DIR" ]; then
+  # Iterate over all private key files in the ~/.ssh/ directory
+  for FILE in "$DIR"/*; do
+    # Basename of the file
+    BASENAME=$(basename "$FILE")
+    # Check if the file does not have the .pub extension and is not named 'config' or 'known_hosts'
+    if [[ "$BASENAME" != *.pub && "$BASENAME" != config && "$BASENAME" != known_hosts* ]]; then
+        # Add SSH key
+        ssh-add "$FILE"
+    fi
+  done
+else
+  echo "The directory $DIR does not exist."
+  exit
+fi
 
 # Start Colima with the SSH agent
 colima start --ssh-agent
-
-# Unset the SSH_AUTH_SOCK variable
-unset SSH_AUTH_SOCK
 
 # Export the SSH_AUTH_SOCK variable
 export SSH_AUTH_SOCK=$(colima ssh env | grep SSH_AUTH_SOCK | cut -d = -f 2)
